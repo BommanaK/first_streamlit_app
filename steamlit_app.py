389c8dfe57121d9496851a71d0803d81b880e255
BommanaK <116809687+BommanaK@users.noreply.github.com>
2022-10-27 18:01:56 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Parents New Healthy Dinner');
-streamlit.header('Breakfast Menu');
-streamlit.text('Pie');
-streamlit.text('Pizza');
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
