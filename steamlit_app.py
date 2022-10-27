457d6b15fd9a113a3c175efe3fa1cf961bb709db
BommanaK <116809687+BommanaK@users.noreply.github.com>
2022-10-27 18:15:31 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
-streamlit.title('🥣 Food Centerr')
-streamlit.header('🥗 Breakfast Menu')
+streamlit.title('Food Centerr')
+streamlit.header('Breakfast Menu')
 streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞 Hard-Boiled Free-Range Egg')
